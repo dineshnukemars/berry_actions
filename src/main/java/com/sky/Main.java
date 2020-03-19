be4e076d389ae25d83f85da836ecef47a7759476
dineshbalasundaram <dinesh.balasundaram@earnin.com>
2020-03-18 21:50:16 -0600

    blink LED one by one completed

--- src/main/java/com/sky/Main.java
+++
@@ -1,9 +0,0 @@
-package com.sky;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Test test=new Test();
-        test.callMe();
-    }
-}
--- src/main/java/com/sky/Test.kt
+++ src/main/java/com/sky/Test.kt
@@ -1,9 +1,36 @@
 package com.sky
 
+import com.pi4j.io.gpio.*
+
 class Test {
 
     fun callMe() {
-        println("hello world")
-        println("hello world 2")
+        GpioFactory.setDefaultProvider(RaspiGpioProvider(RaspiPinNumberingScheme.BROADCOM_PIN_NUMBERING))
+        val gpio = GpioFactory.getInstance()
+        val led1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "Led 1", PinState.LOW)
+        val led2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "Led 2", PinState.LOW)
+        led1.setShutdownOptions(true, PinState.LOW)
+        led2.setShutdownOptions(true, PinState.LOW)
+
+        blink(led1)
+        blink(led2)
+
+        gpio.shutdown()
     }
+
+    private fun blink(led: GpioPinDigitalOutput) {
+        for (i in 0 until 5) {
+            led.low()
+            Thread.sleep(500)
+            println(led.isHigh)
+            led.high()
+            Thread.sleep(500)
+            println(led.isHigh)
+        }
+    }
+}
+
+fun main() {
+    println("blink one by one")
+    Test().callMe()
 }
